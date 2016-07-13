33674b661902984b5a5add014348c0ff2a502c38
reneecampanilla <renee@inspire.co.uk>
2016-07-13 12:07:28 +0800

    modified

---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+//test
+namespace SitePoint;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        print "Heey Ren!";
+    }
+}
