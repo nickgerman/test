a84a8c438d83cd50d2a12a30b4df407813bfd254
reneecampanilla <renee@inspire.co.uk>
2016-07-15 10:19:59 +0800

    updated

--- src/Plugin.php
+++ src/Plugin.php
@@ -59,7 +59,7 @@ class Plugin extends Object implements PluginInterface
         // $this->createTable();
 
         // print_r("Activated.\n\r");
-        $db = \yii\db\Connection();
+        $db = \yii\db\Connection;
         print_r($db);
     }
     /**
