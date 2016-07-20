1bd26f47d69652b662105484cf929b5152f1265b
reneecampanilla <renee@inspire.co.uk>
2016-07-20 10:31:30 +0800

    updated

--- src/Plugin.php
+++ src/Plugin.php
@@ -76,9 +76,7 @@ class Plugin extends Object implements PluginInterface
         // $container->set('yii\db\Connection');
         // $db = $container->get('db');
 
-        print_r("Object->".class_exists('Object')."\n\r");
-        print_r("Connection->".class_exists('Connection')."\n\r");
-        print_r("Component->".class_exists('Component'));
+        print_r(Connection);
         print_r("\n\rActivated.\n\r");
     }
     /**
